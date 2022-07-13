9257fc06a7612c0088a15ac04994d2c55ec7872e
AndresSp <ngxcoder@gmail.com>
2022-07-13 02:51:32 -0500

    feat(components): completed

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -5,20 +5,31 @@ class Forecast extends LitElement {
     return {
         ip: { type: String },
         weather: { type: String },
+        icon: { type: String }
     };
   }
 
-  constructor() {
-    super();
-    this.weather = 'soleado';
+  async willUpdate(){
+    const response = await this.getForecast()
+    const forecast = response?.current?.condition
+    this.weather = forecast?.text
+    this.icon = forecast?.icon
   }
 
-  firstUpdated() {
-
+  async getForecast() {
+    if(!this.ip) {
+      return {}
+    }
+    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=7b1c9b73583e482c8b043712222406&q=${this.ip}&aqi=no`);
+    const obj = await response.json();
+    return obj
   }
 
   render() {
-    return html` <div>${this.weather}</div> `;
+    return html`
+    <div style="margin-bottom: 10px">${this.weather}</div> 
+    <img src=${`https:${this.icon}`} alt="weather">
+    `;
   }
 }
 
--- src/components/Time.js
+++ src/components/Time.js
@@ -1,6 +1,7 @@
 import { LitElement, html } from 'lit';
 
 class Time extends LitElement {
+
   static get properties() {
     return {
       ip: { type: String },
@@ -8,16 +9,17 @@ class Time extends LitElement {
     };
   }
 
-  constructor() {
-    super();
-    this.time = 'app';
-
+  async firstUpdated() {
+    const response = await fetch(`http://worldtimeapi.org/api/ip`);
+    const obj = await response.json();
+    const utcDate = obj.utc_datetime;
+    this.time = new Date(utcDate).toLocaleDateString('es-ES', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
   }
 
   render() {
     return html`
       <main>
-        <h1>${this.ip}</h1>
+        <h1>${this.time}</h1>
       </main>
       </p>
     `;
